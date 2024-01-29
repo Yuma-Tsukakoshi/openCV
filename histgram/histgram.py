1ee933dc03cc74bb93be1e8b1db8c3a4c3ae7e57
Yuma-Tsukakoshi <yttn9984kawa@gmail.com>
2024-01-29 18:12:13 +0900

    add: コメント削除

--- histgram/histgram.py
+++ histgram/histgram.py
@@ -4,24 +4,24 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('img/imori.jpg',0)
 # 第二引数：グレースケールのため[0]
-# hist = cv2.calcHist([img], [0], None, [256], [0, 256])
+hist = cv2.calcHist([img], [0], None, [256], [0, 256])
 
-# # ravel(): 多次元配列を1次元配列に変換する 
-# hist, bins = np.histogram(img.ravel(), 256, [0, 256])
+# ravel(): 多次元配列を1次元配列に変換する 
+hist, bins = np.histogram(img.ravel(), 256, [0, 256])
 
-# # ビンごとの計算はnp.bincount()を使った方が高速
-# hist = np.bincount(img.ravel(), minlength=256)
+# ビンごとの計算はnp.bincount()を使った方が高速
+hist = np.bincount(img.ravel(), minlength=256)
 
-# img = cv2.imread('img/imori.jpg')
-# color = ('b','g','r')
-# for i,col in enumerate(color):
-#     # 第二引数：グレースケールのため[0]
-#     histr = cv2.calcHist([img],[i],None,[256],[0,256])
-#     plt.plot(histr, color = col)
-#     plt.xlim([0,256])
-# plt.show()
+img = cv2.imread('img/imori.jpg')
+color = ('b','g','r')
+for i,col in enumerate(color):
+    # 第二引数：グレースケールのため[0]
+    histr = cv2.calcHist([img],[i],None,[256],[0,256])
+    plt.plot(histr, color = col)
+    plt.xlim([0,256])
+plt.show()
     
-# plt.hist(img.ravel(),256,[0,256]); plt.show()
+plt.hist(img.ravel(),256,[0,256]); plt.show()
 
 #maskを作成する場合
 mask = np.zeros(img.shape[:2], np.uint8)
